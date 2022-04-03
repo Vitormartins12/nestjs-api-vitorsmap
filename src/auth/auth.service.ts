d3de950ecf3373297f66f5817d96b09d66d869cc
Vitormartins12 <vitorsmap@gmail.com>
2022-04-03 11:13:15 -0300

    setting up test dvb

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import {
   ForbiddenException,
   Injectable,
 } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthDTO } from './dto';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
